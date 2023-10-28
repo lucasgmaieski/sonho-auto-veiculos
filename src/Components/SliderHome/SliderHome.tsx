e6d4731e1fee7b0a2fd66971905d03791a14fd06
Administrador <lucasgrigol@gmail.com>
2023-10-27 23:43:28 -0300

    refazendo slider com slick

--- src/Components/SliderHome/SliderHome.tsx
+++ src/Components/SliderHome/SliderHome.tsx
@@ -2,59 +2,54 @@
 
 import api from "@/api";
 import styles from './sliderhome.module.scss'
-import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import { useState, useEffect } from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Link from "next/link";
 
-export default async function SliderHome() {
-    // const menuTipos: any = await api.getMenu(8);
-    // console.log(menuTipos);
-
-    // function getUrl(url: string): string {
-    //     if(process.env.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_WORDPRESS_URL) {
-    //         url = url.replace(process.env.NEXT_PUBLIC_WORDPRESS_URL, process.env.NEXT_PUBLIC_SITE_URL)
-    //         return url;
-    //     }
-    //     return '/';
-    // }
-
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex:any) => {
-        setIndex(selectedIndex);
-    };
+export default function SliderHome() {
     
+
+    function getUrl(url: string): string {
+        if(process.env.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_WORDPRESS_URL) {
+            url = url.replace(process.env.NEXT_PUBLIC_WORDPRESS_URL, process.env.NEXT_PUBLIC_SITE_URL)
+            return url;
+        }
+        return '/';
+    }
+
+    const [menus, setMenus] = useState<any>();
+
+    useEffect(() => {
+        getItensMenu();
+        async function getItensMenu() {
+            const menuTipos: any = await api.getMenu(8);
+            console.log(menuTipos);
+            setMenus(menuTipos);
+        }
+    }, []);
+    
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
     return (
-        <div className="container-fluid">
-            {/* <ul className={`${styles.menuTipos} container-fluid w-100 d-flex justify-content-between align-items-center list-unstyled`}>
-                {menuTipos && menuTipos.itens.map((menu: any, index: number) => (
-                    <li key={index}><Link href={getUrl(menu.url)}>{menu.title}</Link> </li>
+        <div className={`${styles.containerSlider} `}>
+            <Slider {...settings}>
+                {menus && menus.itens.map((menu: any, index: number) => (
+                    <div>
+                        <img src="/banner-home1.jpg" alt="" />
+                        <div>
+                        <h3>{menu.title}</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </div>
+                    </div>
                 ))}
-            </ul> */}
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                    <img src="/banner-home1.jpg" alt="" />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img src="/banner-home1.jpg" alt="" />
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img src="/banner-home1.jpg" alt="" />
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            </Slider>
         </div>
     );
 }
