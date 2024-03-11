f5cf1284ae827a399a9e0e8c4965a418f6a62c7c
Administrador <lucasgrigol@gmail.com>
2024-03-10 23:10:53 -0300

    estilizando componente de paginação

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -1,48 +1,107 @@
-"use client"
-import { useRouter, useSearchParams } from 'next/navigation';
-import { MdOutlineKeyboardDoubleArrowLeft, MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
-
-import React, { useContext, useEffect, useState } from 'react';
-import { Context } from '@/Contexts/Context';
-import { useQueryParams } from '@/lib/utils';
+"use client";
+import {
+    MdOutlineKeyboardDoubleArrowLeft,
+    MdOutlineKeyboardDoubleArrowRight,
+} from "react-icons/md";
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "@/Contexts/Context";
+import { useQueryParams } from "@/lib/utils";
 
 type Props = {
     totalCount: number;
     slug: string;
-    perPage: number
-}
-const Pagination = ({totalCount, slug, perPage} : Props) => {
+    perPage: number;
+};
+
+const Pagination = ({ totalCount, slug, perPage }: Props) => {
     const { urlSearchParams, setQueryParams } = useQueryParams();
-    const { urlParams,changeUrlParams } = useContext(Context);
-    const page = urlSearchParams.get('page') ?? '1'
+    const { urlParams, changeUrlParams } = useContext(Context);
+    const page = urlSearchParams.get("page") ?? "1";
     const [currentPage, setCurrentPage] = useState(Number(page));
 
     useEffect(() => {
-        setCurrentPage(Number(urlSearchParams.get('page') ?? 1))
+        setCurrentPage(Number(urlSearchParams.get("page") ?? 1));
     }, [urlSearchParams]);
 
-    console.log("totalcount: ", totalCount)
+    console.log("totalcount: ", totalCount);
     const totalPages = Math.ceil(Number(totalCount) / Number(perPage));
-  
+
     const onNextPage = () => {
-      setCurrentPage(prevPage => prevPage + 1);
-      setQueryParams({ ['page']: Number(currentPage) + 1 })
-      changeUrlParams(urlSearchParams.toString());
-    };
-  
-    const onPrevPage = () => {
-        setCurrentPage(prevPage => prevPage - 1);
-        setQueryParams({ ['page']: Number(currentPage) > 1 ? Number(currentPage) - 1 : 1 })
+        setQueryParams({ ["page"]: Number(currentPage) + 1 });
         changeUrlParams(urlSearchParams.toString());
     };
-  
-  return (
-    <div className={`flex items-center gap-1 mx-auto mt-5 ${totalCount <= perPage ? 'hidden' : 'block'} `}>
-      <button className='p-2 rounded-lg bg-white text-2xl text-black disabled:opacity-80 disabled:cursor-not-allowed' onClick={onPrevPage} disabled={currentPage === 1}><MdOutlineKeyboardDoubleArrowLeft className="text-black"/></button>
-      <span className='py-1 px-2 rounded-lg bg-white text-2xl text-black'> {currentPage} / {totalPages}</span>
-      <button className='p-2 rounded-lg bg-white text-2xl text-black disabled:opacity-80 disabled:cursor-not-allowed' onClick={onNextPage} disabled={currentPage === totalPages}><MdOutlineKeyboardDoubleArrowRight className="text-black"/></button>
-    </div>
-  );
+
+    const onPrevPage = () => {
+        setQueryParams({
+            ["page"]: Number(currentPage) > 1 ? Number(currentPage) - 1 : 1,
+        });
+        changeUrlParams(urlSearchParams.toString());
+    };
+
+    const onPageChange = (page: number) => {
+        setQueryParams({ ["page"]: page });
+        changeUrlParams(urlSearchParams.toString());
+    };
+
+    return (
+        <div
+            className={`flex gap-1 w-fit mx-auto mt-5 text-xl ${
+                totalCount <= perPage ? "hidden" : "block"
+            }`}
+        >
+            <button
+                className="p-2 rounded-lg bg-gray-300 hover:bg-gray-200 text-black w-[3ch] disabled:opacity-80 disabled:cursor-not-allowed"
+                onClick={onPrevPage}
+                disabled={currentPage === 1}
+            >
+                <MdOutlineKeyboardDoubleArrowLeft className="text-black mx-auto" />
+            </button>
+            {currentPage > 2 && (
+                <button
+                    className="p-2 rounded-lg bg-gray-300 hover:bg-gray-200 text-black w-[3ch]"
+                    onClick={() => onPageChange(currentPage - 2)}
+                >
+                    {currentPage - 2}
+                </button>
+            )}
+            {currentPage > 1 && (
+                <button
+                    className="p-2 rounded-lg bg-gray-300 hover:bg-gray-200 text-black w-[3ch]"
+                    onClick={() => onPageChange(currentPage - 1)}
+                >
+                    {currentPage - 1}
+                </button>
+            )}
+            {currentPage > 0 && (
+                <button className="p-2 rounded-lg bg-blue-600 hover:bg-blue-500 text-white  w-[3ch]">
+                    {currentPage}
+                </button>
+            )}
+            {currentPage < totalPages && (
+                <button
+                    className="p-2 rounded-lg bg-gray-300 hover:bg-gray-200 text-black w-[3ch]"
+                    onClick={() => onPageChange(currentPage + 1)}
+                >
+                    {currentPage + 1}
+                </button>
+            )}
+            {currentPage < totalPages - 1 && (
+                <button
+                    className="p-2 rounded-lg bg-gray-300 hover:bg-gray-200 text-black w-[3ch]"
+                    onClick={() => onPageChange(currentPage + 2)}
+                >
+                    {currentPage + 2}
+                </button>
+            )}
+            <button
+                className="p-2 rounded-lg bg-gray-300 hover:bg-gray-200 text-black w-[3ch] disabled:opacity-80 disabled:cursor-not-allowed"
+                onClick={onNextPage}
+                disabled={currentPage === totalPages}
+            >
+                <MdOutlineKeyboardDoubleArrowRight className="text-black mx-auto" />
+            </button>
+        </div>
+    );
 };
 
 export default Pagination;
